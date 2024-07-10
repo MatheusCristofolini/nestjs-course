fab1c3f0c360461fec41f5f6136b29517cadd487
Matheus Cristofolini <matheuscristofolini@hotmail.com>
2024-07-10 15:38:19 -0300

    Fim do modulo 6 do curso

--- api/src/coffees/coffees.controller.ts
+++ api/src/coffees/coffees.controller.ts
@@ -5,11 +5,15 @@ import {
   Get,
   Inject,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
+  ValidationPipe,
 } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
+import { Protocol } from 'src/common/decorators/protocol.decorator';
+import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
@@ -20,19 +24,24 @@ export class CoffeesController {
   constructor(
     private readonly coffeesService: CoffeesService,
     @Inject(REQUEST) private readonly request: Request,
-  ) {
-    console.log('CoffeesController created');
-  }
+  ) {}
 
+  @Public()
   @Get()
-  findAll(@Query() paginationQuery: PaginationQueryDto) {
-    const { limit, offset } = paginationQuery;
+  async findAll(
+    @Protocol('https') protocol: string,
+    @Query() paginationQuery: PaginationQueryDto,
+  ) {
+    console.log(protocol);
+    //await new Promise((resolve) => setTimeout(resolve, 5000));
+
     return this.coffeesService.findAll(paginationQuery);
     //return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
   }
 
+  @Public()
   @Get(':id')
-  findOne(@Param('id') id: number) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
     return this.coffeesService.findOne('' + id);
     //return 'This action returns ' + id + ' coffee';
   }
@@ -44,7 +53,10 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(
+    @Param('id') id: string,
+    @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto,
+  ) {
     return this.coffeesService.update(id, updateCoffeeDto);
     //return 'This action updates ' + id + ' coffee';
   }
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +17,12 @@ async function bootstrap() {
         enableImplicitConversion: true,
       },
     }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  //app.useGlobalGuards(new ApiKeyGuard());
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
   );
   await app.listen(3000);
 }
